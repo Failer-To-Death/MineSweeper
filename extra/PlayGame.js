58eec6d90cd8e1106fdd439a294bbcbc51c853c8
Failer-To-Death <j8sonhicks@gmail.com>
2024-03-20 12:26:41 -0500

    begining of bomb placing

--- extra/PlayGame.js
+++ extra/PlayGame.js
@@ -1,22 +1,50 @@
+const bombs = 25
+const rows = 10
+const columns = 10
 
-function makeRows(rows, cols) {
+
+function placeBombs(){
+    for (i=0; i<25; i++){
+        let max = 99;
+        let pick = Math.floor(Math.random() * max);
+        let c = document.getElementById(pick)
+        c.innerText = ' '
+        //if (cell.innerText = ' '){   
+        //    i= i-1;
+        //}else{
+        
+        //}
+    }
+}
+
+function makeRows(rows, columns) {
     container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
+    container.style.setProperty('--grid-cols', columns);
+    for (c = 0; c < (rows * columns); c++) {
         let cell = document.createElement("div");
         //number cells 
         cell.innerText = (c + 1);
         cell.id = (c)
         container.appendChild(cell).className = "grid-item";
         clickCell(cell);
+        
     };
+    placeBombs()
 };
 
-
+/* attempt to get a bomb img
+        var img = document.createElement("img");
+        img.src ="bomb.png";
+        img.width = 10;
+        img.height = 10;
+        img.alt = 'bomb';
+        c.appendChild(img)
+*/
 
 function clickCell(c) {
     c.addEventListener('click', function(event) {
         this.style.backgroundColor = 'lightGrey';
+        c.innerText = ' '
     });
 }
 
@@ -33,6 +61,6 @@ para.innerText = "This was created";
 document.body.appendChild(para);*/
 
 const container = document.getElementById("grid");
-makeRows(10, 10);
+makeRows(rows, columns);
 
 
